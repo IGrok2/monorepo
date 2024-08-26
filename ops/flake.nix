192e48ccb1136029ec03675dfceaf7107cefccfa
Edward C <76141700+edwardwc@users.noreply.github.com>
2024-08-25 21:26:55 -0700

    ci

--- ops/flake.nix
+++ ops/flake.nix
@@ -47,7 +47,13 @@
             # for opnieuw
             rustc
             cargo
+            rustup
           ];
+
+          shellHook = ''
+            rustup default stable
+            rustup component add clippy
+          '';
         };
       });
     };
